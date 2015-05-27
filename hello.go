d8e2ac3531001436e0cf31b12ced31c9c02c0628
Brian Busche <brian@rendition.net>
2015-05-27 10:19:55 -0500

    updated hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+"fmt"
+"github.com/bbusche/stringutil"
+)
 
 func main() {
-    fmt.Printf("Hello, world.\n")
+    //fmt.Printf("Hello, world.\n")
+    fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
